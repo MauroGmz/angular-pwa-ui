a01193f1c10e769ca30d12ea8824c436549c1b58
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-08-16 20:02:53 -0300

    adding use of the camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    {id: 1, name: "Mauro", country: "USA"},
+    {id: 2, name: "Franco", country: "Francia"},
+    {id: 3, name: "Gonzalo", country: "Portugal"},
+    {id: 4, name: "Dembele", country: "Francia"}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if(this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  delete() {
+    if(confirm("Are you sure want to delete it?")) {
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
+
 }
