968b6d8e4dc199e35f49457110905fc8d9ed2be9
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-08-17 20:44:49 -0300

    adding camera pwa component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CameraComponentComponent } from './camera-component/camera-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/camera-component/camera-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-component',
+  templateUrl: './camera-component.component.html',
+  styleUrls: ['./camera-component.component.css']
+})
+export class CameraComponentComponent implements OnInit {
+
+  title = 'Camera PWA Section';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
